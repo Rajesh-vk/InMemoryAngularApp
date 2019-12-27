a2c32fe27ed5d9ee0b06780f93aac23a74a56bba
test <test@gmail.com>
2019-12-27 16:20:17 +0530

    Inmemory commit

---
+++ src/app/Model/eventSummay.ts
@@ -0,0 +1,23 @@
+export interface EventSummary {
+            eventID: string;
+            month: string;
+            baseLocation: string;
+            beneficiaryName: string;
+            venueAddress: string;
+            councilName: string;
+            project: string;
+            category: string;
+            eventName: string;
+            eventDescription: string;
+            eventDate: string;
+            totalNoVolunteers: string;
+            totalVolunteHours: string;
+            totalTravelHours: string;
+            overallVolunteeringHours: string;
+            livesImpacted: string;
+            activityType: string;
+            status: string;
+            pocID: string;
+            pocName: string;
+            pocContactNumber: string;
+}
