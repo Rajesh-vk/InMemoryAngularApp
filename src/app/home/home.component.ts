b574a4f6a8f265f536c3623b033e730f6c1e1b1d
Rajesh-vk <58765073+Rajesh-vk@users.noreply.github.com>
2020-01-03 13:31:56 +0530

    home service commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './home.service';
+import { Subject, EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   pageTitle = 'DashBoard';
-  constructor() { }
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+  eventSummary$ = this.homeService.eventsSummary$
+    .pipe(
+      catchError(err => {
+        this.errorMessageSubject.next(err);
+        return EMPTY;
+      })
+    );
+  constructor(private homeService: HomeService) { }
 
   ngOnInit() {
   }
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, combineLatest, EMPTY, from, merge, Subject, throwError, of } from 'rxjs';
+import { catchError, filter, map, mergeMap, scan, shareReplay, tap, toArray, switchMap } from 'rxjs/operators';
+import { EventSummary } from '../Model/eventSummay';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
+  private eventsSummaryUrl = 'api/eventSummarys';
 
-  constructor() { }
+  eventsSummary$ = this.httpUrl.get<EventSummary[]>(this.eventsSummaryUrl)
+    .pipe(
+      tap(data => console.log('EventsSummary', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  constructor(private httpUrl: HttpClient, ) { }
+
+  private handleError(err: any) {
+    let errorMessage: string;
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    }
+    console.error(err);
+    return throwError(errorMessage);
+  }
 }
