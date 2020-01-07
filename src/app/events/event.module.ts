c00dfbc8bed79e25d9af981b43dffae0850f16f1
test <test@gmail.com>
2020-01-07 12:16:14 +0530

    shared Module commit

--- src/app/events/event.module.ts
+++ src/app/events/event.module.ts
@@ -3,12 +3,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { AuthGuard } from '../user/auth.guard';
 import { EventsComponent } from './events.component';
-import { CommonModule } from '../commonComponent/common.module';
 
 @NgModule({
   imports: [
     SharedModule,
-    CommonModule,
     RouterModule.forChild([
         {
             path: 'event',
