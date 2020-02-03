03bac267af45273e4f97203eb29152d92f95201b
test <test@gmail.com>
2020-02-03 18:48:29 +0530

    Event volunteers commit.

--- src/app/events/event-edit-volundeers/event-edit-volundeers.component.ts
+++ src/app/events/event-edit-volundeers/event-edit-volundeers.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { EventSummary, EventResolved } from 'src/app/Model/eventSummay';
 
 @Component({
   selector: 'app-event-edit-volundeers',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventEditVolundeersComponent implements OnInit {
 
-  constructor() { }
+  
 
-  ngOnInit() {
+  errorMessage: string;
+  event: EventSummary;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.parent.data.subscribe(data => {
+      const resolvedData: EventResolved = data['resolvedData'];
+      this.errorMessage = resolvedData.error;
+      this.event = resolvedData.eventSummary;
+      console.log(1);
+      console.log(this.event);
+    });
   }
 
 }
